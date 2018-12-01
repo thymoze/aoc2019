41af857314f4175373b09f28b071d5337398e3dd
David Heim <davidrh@live.de>
2018-12-01 18:30:01 +0100

    Clean up and improve

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -3,25 +3,25 @@ extern crate aoc2018;
 use std::collections::HashSet;
 
 fn main() {
-    let input: Vec<i32> = aoc2018::input(1).lines()
-        .map(|line| line.parse::<i32>().unwrap())
+    let input: Vec<isize> = aoc2018::input(1).lines()
+        .filter_map(|line| line.parse().ok())
         .collect();
     
     // Part 1
-    println!("Resulting frequency: {}", input.iter().sum::<i32>());
+    println!("Resulting frequency: {}", input.iter().sum::<isize>());
 
     // Part 2
-    let mut seen: HashSet<i32> = HashSet::with_capacity(input.len());
-    let mut curr_freq = 0;
-    seen.insert(curr_freq);
+    let mut seen = HashSet::new();
+    seen.insert(0);
+    
+    let mut curr = 0;
 
-    for change in input.iter().cycle() {
-        curr_freq += change;
+    input.iter().cycle()
+        .take_while(|x| {
+            curr += **x;
+            seen.insert(curr)
+        }).last();
 
-        if !seen.insert(curr_freq) {
-            break;
-        }
-    }
-    println!("First frequency reached twice: {}", curr_freq);
+    println!("First frequency reached twice: {}", curr);
 
 }
